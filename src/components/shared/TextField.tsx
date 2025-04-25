02af16fb6bc3480ad414cec21699e18715643218
Ehtesham zahraee <ehtesham.dev@gmail.com>
2025-04-25 11:49:31 +0330

    feat: implement enter keyup in text-field component

--- src/components/shared/TextField.tsx
+++ src/components/shared/TextField.tsx
@@ -4,13 +4,21 @@ import { TextFieldProps } from "@/types/components/TextField.ts";
 import { IconLoader } from "@/components/shared/IconLoader.tsx";
 
 export const TextField: React.FC<TextFieldProps> = ({
+  model = "",
   prependIcon,
   appendIcon,
   name,
   placeholder,
   type,
-  onInputHandler,
+  onInputHandler = () => {},
+  onEnterHandler = () => {},
 }) => {
+  const handleEnterKeyUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key !== "Enter") return;
+
+    onEnterHandler(event.currentTarget.value);
+  };
+
   return (
     <div className="text-field">
       {prependIcon && (
@@ -20,10 +28,12 @@ export const TextField: React.FC<TextFieldProps> = ({
       )}
       <input
         id={name}
+        value={model}
         className="text-field__field"
         type={type}
         placeholder={placeholder}
         onInput={onInputHandler}
+        onKeyUp={handleEnterKeyUp}
       />
       {appendIcon && (
         <IconLoader name={appendIcon} className="text-field__append-icon">
--- src/types/components/TextField.ts
+++ src/types/components/TextField.ts
@@ -1,14 +1,17 @@
 import { ChangeEvent } from "react";
 
 type ValidFieldTypes = "text" | "number" | "password";
+type ValidModelTypes = string | number;
 
 export interface TextFieldProps {
   type?: ValidFieldTypes;
   name: string;
+  model?: ValidModelTypes;
   placeholder?: string;
   prependIcon?: string;
   appendIcon?: string;
   isDisabled?: boolean;
 
-  onInputHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+  onInputHandler?: (event: ChangeEvent<HTMLInputElement>) => void;
+  onEnterHandler?: (value: ValidModelTypes) => void;
 }
